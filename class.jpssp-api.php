28ffd048fa5b591623445bb5f255219c6efb2a4c
Daisuke Takahashi <daisuke@extendwings.com>
2014-10-24 03:37:51 +0900

    Add Google+ Counter Class

--- class.jpssp-api.php
+++ class.jpssp-api.php
@@ -1,10 +1,9 @@
 <?php
 
-$instance = Feedly_API::init();
+$instance[] = Feedly_API::init();
+$instance[] = Google_API::init();
 
 abstract class JPSSP_API {
-	static $instance;
-
 	const OPTION_NAME_ACTIVATED = 'jpssp-api_activated';
 	const API_ENDPOINT = 'jpssp-api';
 
@@ -49,6 +48,7 @@ abstract class JPSSP_API {
 
 	public function add_rewrite_endpoint() {
 		add_rewrite_endpoint( static::API_ENDPOINT, EP_ROOT );
+		add_rewrite_endpoint( Google_API::API_ENDPOINT, EP_ROOT );
 	}
 
 	public function query_vars( $vars ) {
@@ -107,6 +107,73 @@ class Feedly_API extends JPSSP_API {
 			echo $callback . '(';
 			if( !is_wp_error( $response ) && $status == 200 ) {
 				echo json_encode( $body, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE );
+			} else {
+				status_header( $status );
+				echo json_encode( array(
+					'meta' => array(
+						'code'    => $status,
+						'message' => wp_remote_retrieve_response_message( $response ),
+					),
+				) );
+			}
+			echo ');';
+			exit;
+		}
+	}
+}
+
+class Google_API extends JPSSP_API {
+	static $instance;
+
+	const API_ENDPOINT = 'google-api';
+
+	public function template_redirect() {
+		global $wp_query;
+		trigger_error("called google", E_USER_NOTICE);
+		if( is_object( $wp_query ) && isset( $wp_query->query[ self::API_ENDPOINT ] ) ) {
+			if( !empty( $_GET['url'] ) ) {
+				$url = $_GET['url'];
+			} else {
+				$url = home_url();
+			}
+
+			$transient_name = 'jpssp-google-api_' . hash( 'crc32b', $url );
+
+			if( ( $response = get_transient( $transient_name ) ) === false ) {
+				$response = wp_remote_post( 'https://clients6.google.com/rpc', array(
+					'httpversion' => '1.1',
+					'body'        => '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $url . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]',
+					'headers'     => array( 'Content-Type' => 'application/json' ),
+				) );
+				$status   = wp_remote_retrieve_response_code( $response );
+
+				if( !is_wp_error( $response ) && $status == 200 ) {
+					set_transient( $transient_name, $response, HOUR_IN_SECONDS );
+				}
+			} else {
+				$status = wp_remote_retrieve_response_code( $response );
+			}
+
+			$result = json_decode( $response, true );
+			$result = $result[0];
+
+			if( isset( $result['error'] ) || !isset( $result['result']['metadata']['globalCounts']['count'] ) ) {
+				$count = 0;
+			} else {
+				$count = intval( $result['result']['metadata']['globalCounts']['count'] );
+			}
+
+			nocache_headers();
+			header('Content-Type: application/javascript; charset=UTF-8');
+
+			$callback = 'update_google_count';
+			if( !empty( $_GET['callback'] ) ) {
+				$callback = esc_js( $_GET['callback'] );
+			}
+
+			echo $callback . '(';
+			if( !is_wp_error( $response ) && $status == 200 ) {
+				echo json_encode( $count, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE );
 			} else {
 				status_header( $status );
 				echo json_encode( array(
